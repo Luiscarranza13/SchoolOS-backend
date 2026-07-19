8e1d24a94756e1a0e4118bf67ca74e1789fd2660
Luis Armando <180464844+Luiscarranza13@users.noreply.github.com>
2026-07-19 13:42:11 -0500

    use portable rate limit import

--- src/middlewares/rateLimit.middleware.ts
+++ src/middlewares/rateLimit.middleware.ts
@@ -1,4 +1,4 @@
-import rateLimit from "express-rate-limit";
+import { rateLimit } from "express-rate-limit";
 import type { Options } from "express-rate-limit";
 
 const loginRateLimitOptions: Partial<Options> = {
